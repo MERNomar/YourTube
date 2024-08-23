822f5acccf5689a7c510277999e84ade73aab5c4
Omar Ahmed <109013442+MERNomar@users.noreply.github.com>
2024-08-23 19:15:30 +0300

    Added video and audio switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { downloadYoutube } from "./youtubeDownloader";
 import Modal from "./components/Modal";
 
 function App() {
-  const [link, setLink] = useState("");
-
+  const [downloadLink, setDownloadLink] = useState("");
+  const [mediaLink, setMediaLink] = useState("");
+  const [isAudioOnly, setIsAudioOnly] = useState(false);
+  useEffect(() => {
+    console.log(isAudioOnly);
+  }, [isAudioOnly]);
   const startDownload = async () => {
-    const link = await downloadYoutube(
-      "https://www.youtube.com/watch?v=C0DPdy98e4c",
-    );
-    setLink(link);
+    const link = await downloadYoutube(mediaLink, isAudioOnly);
+    setDownloadLink(link);
   };
 
   return (
@@ -20,17 +22,33 @@ function App() {
             <img src="/yourTubeLogo.svg" alt="logo" />
           </header>
           <div className="flex justify-center">
-            <input
-              type="text"
-              className="w-full h-8 pl-1 pr-1 mr-1 font-bold border-2 border-gray-400 border-solid rounded-md outline-gray-600 "
-            />
+            <div className="relative w-full mr-1">
+              <input
+                onChange={(e) => setMediaLink(e.target.value)}
+                value={mediaLink}
+                type="text"
+                className="w-full h-8 pl-20 pr-1 mr-1 font-bold border-2 border-gray-400 border-solid rounded-md outline-none outline-1 focus:outline-gray-400"
+              />
+              <select
+                className="absolute top-[7%]  left-[5px] border-solid border-2 border-gray-400"
+                name="is-audio"
+                id="is-audio"
+                onChange={(e) => {
+                  if (e.target.value === "Video") setIsAudioOnly(false);
+                  if (e.target.value === "Audio") setIsAudioOnly(true);
+                }}
+              >
+                <option value={"Video"}>Video</option>
+                <option value={"Audio"}>Audio</option>
+              </select>
+            </div>
             <button
               className="pl-1 pr-1 font-medium transition-colors duration-75 bg-gray-400 rounded-md hover:bg-gray-500"
               onClick={() => startDownload()}
             >
               Download
             </button>
-            {link !== "" && <Modal link={link} />}
+            {downloadLink !== "" && <Modal link={downloadLink} />}
           </div>
         </div>
       </div>
--- src/youtubeDownloader.ts
+++ src/youtubeDownloader.ts
@@ -7,9 +7,13 @@ const headers = {
   Accept: "application/json",
 };
 
-const downloadYoutube = async (url: string) => {
+const downloadYoutube = async (url: string, isAudioOnly: boolean) => {
   try {
-    const { data } = await axios.post(copaltURL, { url }, { headers });
+    const { data } = await axios.post(
+      copaltURL,
+      { url, isAudioOnly },
+      { headers },
+    );
     return data.url;
   } catch (err) {
     console.log(err);
