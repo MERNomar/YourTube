f330b45c4257cd4928943d3699be3f2171242c5f
Omar Ahmed <109013442+MERNomar@users.noreply.github.com>
2024-08-23 20:34:59 +0300

    YourTube 0.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,44 @@
 import { useEffect, useState } from "react";
 import { downloadYoutube } from "./youtubeDownloader";
-import Modal from "./components/Modal";
 
 function App() {
-  const [downloadLink, setDownloadLink] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [inputError, setInputError] = useState<{ errorMessage: any }>({
+    errorMessage: "",
+  });
   const [mediaLink, setMediaLink] = useState("");
   const [isAudioOnly, setIsAudioOnly] = useState(false);
+
   useEffect(() => {
     console.log(isAudioOnly);
   }, [isAudioOnly]);
+
   const startDownload = async () => {
-    const link = await downloadYoutube(mediaLink, isAudioOnly);
-    setDownloadLink(link);
+    try {
+      if (mediaLink === "") {
+        throw new Error("No Link Provided");
+      }
+      const urlValidator = new URL(mediaLink);
+
+      if (urlValidator.host !== "www.youtube.com") {
+        throw new Error(
+          "Currently Only youtube is supported, be tuned for new updates",
+        );
+      }
+      setInputError({ errorMessage: "" });
+      setLoading(true);
+      const link = await downloadYoutube(mediaLink, isAudioOnly);
+      const url = document.createElement("a");
+      url.href = link;
+      url.setAttribute("download", "");
+      console.log(url);
+      url.click();
+      setLoading(false);
+      setMediaLink("");
+    } catch (err) {
+      setMediaLink("");
+      setInputError({ errorMessage: err });
+    }
   };
 
   return (
@@ -25,9 +52,10 @@ function App() {
             <div className="relative w-full mr-1">
               <input
                 onChange={(e) => setMediaLink(e.target.value)}
+                placeholder={inputError.errorMessage}
                 value={mediaLink}
                 type="text"
-                className="w-full h-8 pl-20 pr-1 mr-1 font-bold border-2 border-gray-400 border-solid rounded-md outline-none outline-1 focus:outline-gray-400"
+                className={`placeholder:text-[#ff00007a]  w-full h-8 pl-20 pr-1 mr-1 font-bold border-2 border-gray-400 border-solid rounded-md outline-none outline-1 focus:outline-gray-400 ${inputError.errorMessage !== "" && "outline-red-600 outline-2"}`}
               />
               <select
                 className="absolute top-[7%]  left-[5px] border-solid border-2 border-gray-400"
@@ -45,10 +73,10 @@ function App() {
             <button
               className="pl-1 pr-1 font-medium transition-colors duration-75 bg-gray-400 rounded-md hover:bg-gray-500"
               onClick={() => startDownload()}
+              disabled={loading ? true : false}
             >
-              Download
+              {loading ? "loading..." : "Download"}
             </button>
-            {downloadLink !== "" && <Modal link={downloadLink} />}
           </div>
         </div>
       </div>
