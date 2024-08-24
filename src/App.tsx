f882b237f91ca5c82c5dbfe8f324d305aa53b487
Omar Ahmed <109013442+MERNomar@users.noreply.github.com>
2024-08-24 18:50:44 +0300

    added _blank to links

--- src/App.tsx
+++ src/App.tsx
@@ -92,12 +92,14 @@ function App() {
             <a
               className="font-bold text-gray-800 hover:underline"
               href="https://www.linkedin.com/in/omar-ahmed-a4ab79244/"
+              target="_blank"
             >
               MERNOmar
             </a>
           </div>
           <div className="flex mr-3">
             <a
+              target="_blank"
               href="https://www.linkedin.com/in/omar-ahmed-a4ab79244/"
               className="w-5 h-5 mr-2 text-lg"
             >
